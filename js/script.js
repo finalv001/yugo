19b2fc25796d8a85df841df90ade5dcb5e7fe562
Caroline Gritsch <e11831765@student.tuwien.ac.at>
2024-04-01 20:10:31 +0200

    improved mobile performance

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function clearCanvas() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-
 function resize() {
   canvas.width = window.innerWidth * 2;
   canvas.height = window.innerHeight * 2;
@@ -33,27 +32,27 @@ document.addEventListener("mousemove", draw);
 
 function draw(e) {
   context.font = 'bold 32px "Times New Roman", Times, serif';
-  context.fillStyle = "#000"; 
-  context.strokeStyle = "#fff"; 
-  context.lineWidth = 1; 
+  context.fillStyle = "#000";
+  context.strokeStyle = "#fff";
+  context.lineWidth = 1;
 
   context.fillText("YUGO XYZ", e.clientX, e.clientY);
   context.strokeText("YUGO XYZ", e.clientX, e.clientY);
 }
 function createWord() {
-    var word = document.createElement("div");
-    word.className = "word";
-    word.style.position = "absolute";
-    word.style.top = "-50px";
-    word.style.left =
-      Math.random() * document.querySelector("#rain-container").offsetWidth +
-      "px";
-    word.style.top = "-50px";
-    word.style.fontSize = "24px";
-    word.style.color = "black";
-    word.style.strokeStyle = "#fff";
-    word.innerText = "YUGO XYZ";
-    word.style.textShadow = `
+  var word = document.createElement("div");
+  word.className = "word";
+  word.style.position = "absolute";
+  word.style.top = "-50px";
+  word.style.left =
+    Math.random() * document.querySelector("#rain-container").offsetWidth +
+    "px";
+  word.style.top = "-50px";
+  word.style.fontSize = "24px";
+  word.style.color = "black";
+  word.style.strokeStyle = "#fff";
+  word.innerText = "YUGO XYZ";
+  word.style.textShadow = `
     1px 0 0 #fff,
    -1px 0 0 #fff,
     0 1px 0 #fff,
@@ -61,41 +60,43 @@ function createWord() {
     2px 2px #fff,
     -2px -2px 0 #fff,
     2px -2px 0 #fff,
-    -2px 2px 0 #fff`; 
+    -2px 2px 0 #fff`;
 
-    var rotation = Math.random() * 30 - 15; 
-    word.style.transform = `rotate(${rotation}deg)`;
-    document.querySelector("#rain-container").appendChild(word);
-    word.dataset.speed = Math.random() * 2 + 1; 
-  }
-
-
-function moveWords() {
-    var words = document.querySelectorAll(".word");
-    words.forEach(function (word) {
-      var topPos = parseFloat(word.style.top);
-      var speed = parseFloat(word.dataset.speed);
-      var newPos = topPos + speed;
-      word.style.top = newPos + "px";
+  var rotation = Math.random() * 30 - 15;
+  word.dataset.speed = Math.random() * 2 + 1;
+  word.style.transform = `rotate(${rotation}deg)`;
+  word.classList.add("word-out-of-view"); 
+  document.querySelector("#rain-container").appendChild(word);
   
-      if (newPos > document.querySelector("#rain-container").offsetHeight) {
-        word.style.top =
-          document.querySelector("#rain-container").offsetHeight -
-          word.offsetHeight +
-          "px";
-      }
-    });
-  }
-
-if (window.innerWidth >= 600) {
-  document.addEventListener("mousemove", draw);
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "c") {
-      clearCanvas();
-    }
-  });
-} else {
-  setInterval(createWord, 200);
-  setInterval(moveWords, 20);
 }
 
+function moveWords() {
+    var words = document.querySelectorAll(".word-out-of-view");
+    words.forEach(function (word) {
+        var topPos = parseFloat(word.style.top);
+        var speed = parseFloat(word.dataset.speed);
+        var newPos = topPos + speed;
+        word.style.top = newPos + "px";
+        
+        if (newPos > window.innerHeight) {
+            word.remove(); 
+        }
+    });
+
+    requestAnimationFrame(moveWords);
+}
+
+if (window.innerWidth < 600) {
+    setInterval(createWord, 400); 
+    moveWords(); 
+  }if (window.innerWidth >= 600) {
+    document.addEventListener("mousemove", draw);
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "c") {
+        clearCanvas();
+      }
+    });
+  } else {
+    setInterval(createWord, 200); 
+    moveWords(); 
+  }
